e832c551c1e0107c1023d078da678d219d1fc3c2
Parasire-010 <141277907+Parasire-010@users.noreply.github.com>
2025-09-06 12:30:37 -0600

    chore: clean project (remove Room schemas) + updates

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.geoquiz
 
 import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
